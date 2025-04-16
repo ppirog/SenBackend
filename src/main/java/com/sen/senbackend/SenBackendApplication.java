a045da6768f473b96a48bf348b156695bdf46ee7
pawel <pawelpirog99@gmail.com>
2025-04-16 14:37:15 +0200

    setup config

---
+++ src/main/java/com/sen/senbackend/SenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sen.senbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenBackendApplication.class, args);
+    }
+
+}
